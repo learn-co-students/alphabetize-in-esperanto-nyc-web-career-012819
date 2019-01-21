3c0a6576a3354ddcb350cd615a9a29ba849565f8
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-21 01:13:15 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  arr.sort_by! do |a|
+    new_array = []
+    a.each_char do |char|
+      new_array << ESPERANTO_ALPHABET.index(char)
+    end
+    new_array
+  end
+  arr
 end
