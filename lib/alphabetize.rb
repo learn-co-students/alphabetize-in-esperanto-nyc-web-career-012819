aa3504d6e819d1a4628d16443a215053480f94eb
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-01-21 14:18:59 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |sentence|
+    sentence.split("").map do |letter|
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
