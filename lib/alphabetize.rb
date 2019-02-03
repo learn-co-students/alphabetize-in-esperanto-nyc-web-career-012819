6268275db9b25b74ef0675d3b24538e4efecf8bc
Ethan Nam <ethan.s.nam@gmail.com>
2019-02-03 10:34:50 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+	
+	arr.sort_by do |word|
+		word.split('').map { |letter| ESPERANTO_ALPHABET.index(letter) }
+	end
+
 end
